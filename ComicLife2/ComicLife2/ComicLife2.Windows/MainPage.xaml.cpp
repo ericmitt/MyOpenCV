9c12f5fd34f047659c0e727f3eb1ee1c722fd6fd
unknown <ericmitt@MININT-KJ51B7H.redmond.corp.microsoft.com>
2015-04-28 13:52:05 -0700

    Adding ComicLife2

---
+++ ComicLife2/ComicLife2/ComicLife2.Windows/MainPage.xaml.cpp
@@ -0,0 +1,400 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <Robuffer.h> //comptr etc
+#include <opencv2\core\core.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\features2d\features2d.hpp>
+#include <opencv2\imgproc.hpp>
+
+#include <opencv2\imgproc\types_c.h>
+#include <opencv2\imgcodecs\imgcodecs.hpp>
+
+
+
+
+using namespace ComicLife2;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+using namespace Windows::Storage::Streams;
+using namespace Windows::UI::Xaml::Media::Imaging;
+using namespace Windows::Graphics::Imaging;
+using namespace Microsoft::WRL;
+using namespace concurrency;
+using namespace Windows::Storage::Streams;
+
+using namespace Windows::Storage;
+using namespace Windows::Storage::Pickers;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+ComicLife2::MainPage::MainPage()
+{
+	InitializeComponent();
+
+	m_bPreviewing = false;
+	mode = -1;
+}
+
+
+
+Uri^ InputImageUri = ref new Uri(L"ms-appx:///Assets/stars.png");
+void ComicLife2::MainPage::LoadImage()
+{
+	RandomAccessStreamReference^ streamRef = RandomAccessStreamReference::CreateFromUri(InputImageUri);
+
+	task<IRandomAccessStreamWithContentType^>(streamRef->OpenReadAsync()).
+		then([](task<IRandomAccessStreamWithContentType^> thisTask)
+	{
+		IRandomAccessStreamWithContentType^ fileStream = thisTask.get();
+		return BitmapDecoder::CreateAsync(fileStream);
+	}).
+		then([](task<BitmapDecoder^> thisTask)
+	{
+		BitmapDecoder^ decoder = thisTask.get();
+		return decoder->GetFrameAsync(0);
+	}).
+		then([this](task<BitmapFrame^> thisTask)
+	{
+		BitmapFrame^ frame = thisTask.get();
+
+		// Save some information as fields
+		frameWidth = frame->PixelWidth;
+		frameHeight = frame->PixelHeight;
+
+		return frame->GetPixelDataAsync();
+	}).
+		then([this](task<PixelDataProvider^> thisTask)
+	{
+		PixelDataProvider^ pixelProvider = thisTask.get();
+		Platform::Array<byte>^ srcPixels = pixelProvider->DetachPixelData();
+		Lena = cv::Mat(frameHeight, frameWidth, CV_8UC4);
+		memcpy(Lena.data, srcPixels->Data, 4 * frameWidth*frameHeight);
+		UpdateImage(Lena);
+	});
+
+}
+
+void MainPage::UpdateImage(cv::Mat image)
+{
+	
+		// Create the WriteableBitmap
+		WriteableBitmap^ bitmap = ref new WriteableBitmap(image.cols, image.rows);
+
+		// Get access to the pixels
+		IBuffer^ buffer = bitmap->PixelBuffer;
+		unsigned char* dstPixels;
+
+		// Obtain IBufferByteAccess
+		ComPtr<IBufferByteAccess> pBufferByteAccess;
+		ComPtr<IUnknown> pBuffer((IUnknown*)buffer);
+		pBuffer.As(&pBufferByteAccess);
+
+		// Get pointer to pixel bytes
+		pBufferByteAccess->Buffer(&dstPixels);
+		memcpy(dstPixels, image.data, 4 * image.cols*image.rows);
+
+		// Set the bitmap to the Image element
+		img->Source = bitmap; 
+
+}
+
+
+
+void ComicLife2::MainPage::btn1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Load Image
+	
+	//LoadImage();
+
+	cv::Mat image = cv::imread("Assets/stars.png");
+	Lena = cv::Mat(image.rows, image.cols, CV_8UC4);
+	cvtColor(image, Lena, CV_BGR2BGRA);
+	UpdateImage(Lena);
+}
+
+cv::Mat result;
+void ComicLife2::MainPage::btn2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//apply effect on Image
+	
+	cv::Mat intermediateMat;
+	
+	cv::cvtColor(Lena, intermediateMat, CV_RGBA2GRAY);
+	cv::cvtColor(intermediateMat, result, CV_GRAY2BGRA);
+	
+	//or apply a filter
+	//cv::Mat result;
+	//cv::Canny(Lena, intermediateMat, 80, 90);
+	//cv::cvtColor(intermediateMat, result, CV_GRAY2BGRA);
+
+	UpdateImage(result);
+}
+
+cv::String ComicLife2::MainPage::ConvertPath(Platform::String^ path) {
+	std::wstring localPathW(path->Begin());
+
+	// Opt #1
+	//std::string localPath(localPathW.begin(), localPathW.end());
+
+	// Opt #2
+	//std::string localPath(StrToWStr(localPathW));
+
+	// Opt #3
+	size_t outSize = localPathW.length() + 1;
+	char* localPathC = new char[outSize];
+	size_t charsConverted = 0;
+	wcstombs_s(&charsConverted, localPathC, outSize, localPathW.c_str(), localPathW.length());
+	cv::String localPath(localPathC);
+
+	// Implicit conversion from std::string to cv::String
+	return localPath;
+}
+
+void ComicLife2::MainPage::btn3_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+	//Save Image
+	StorageFolder^ localFolderRT = ApplicationData::Current->LocalFolder;
+	cv::String localFile = ConvertPath(ApplicationData::Current->LocalFolder->Path) + "\\MyLena.png";
+	auto x = cv::imwrite(localFile, result);
+
+	
+	
+} 
+
+void ComicLife2::MainPage::ShowStatusMessage(Platform::String^ text)
+{
+	txtMsg->Text += "\n" + text;
+}
+
+void ComicLife2::MainPage::EnumerateWebcamsAndInitializeFirstOneAsync()
+{
+	try
+	{
+		ShowStatusMessage("Enumerating Webcams...");
+		m_devInfoCollection = nullptr;
+
+		task<DeviceInformationCollection^>(DeviceInformation::FindAllAsync(DeviceClass::VideoCapture)).then([this](task<DeviceInformationCollection^> findTask)
+		{
+			try
+			{
+				m_devInfoCollection = findTask.get();
+				if (m_devInfoCollection == nullptr || m_devInfoCollection->Size == 0)
+				{
+					ShowStatusMessage("No WebCams found.");
+				}
+				else
+				{
+					for (unsigned int i = 0; i < m_devInfoCollection->Size; i++)
+					{
+						auto devInfo = m_devInfoCollection->GetAt(i);
+						ShowStatusMessage(devInfo->Name);
+					}
+					ShowStatusMessage("Enumerating Webcams completed successfully.");
+					
+				}
+			}
+			catch (Exception ^e)
+			{
+				ShowStatusMessage(e->Message);
+			}
+		}).then([this](){
+		
+			auto mediaCapture = ref new Windows::Media::Capture::MediaCapture();
+			m_mediaCaptureMgr = mediaCapture;
+			auto settings = ref new Windows::Media::Capture::MediaCaptureInitializationSettings();
+			auto devInfo = m_devInfoCollection->GetAt(0);
+			settings->VideoDeviceId = devInfo->Id;
+
+			create_task(mediaCapture->InitializeAsync(settings)).then([this](task<void> initTask)
+			{
+				try
+				{
+					initTask.get();
+					ShowStatusMessage("Initialize done");
+				}
+				catch (Exception ^ e)
+				{
+					ShowStatusMessage(e->Message);
+				}
+			});
+
+		});
+	}
+	catch (Platform::Exception^ e)
+	{
+		ShowStatusMessage(e->Message);
+	}
+}
+
+
+void ComicLife2::MainPage::btn4_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	EnumerateWebcamsAndInitializeFirstOneAsync();
+}
+
+
+void ComicLife2::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//tmpFct();
+
+	/*StorageFile localfile = ApplicationData::Current->LocalFolder->;
+	auto encoder = ref new  Windows::Media::MediaProperties::ImageEncodingProperties();
+	encoder->CreateJpeg;
+
+	preview1->Source->CapturePhotoToStorageFileAsync(encoder, localFile);*/
+
+}
+
+
+
+void ComicLife2::MainPage::Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Effect on Video
+	//Will load the dll
+	auto mediaCapture = m_mediaCaptureMgr.Get();
+	
+	
+	if (m_bPreviewing == false){
+
+		preview1->Source = mediaCapture;
+		create_task(mediaCapture->StartPreviewAsync()).then([this](task<void> previewTask)
+		{
+			try
+			{
+				previewTask.get();
+				m_bPreviewing = true;
+				ShowStatusMessage("Start preview successful");
+			}
+			catch (Exception ^e)
+			{
+				ShowStatusMessage(e->Message);
+			}
+		});
+		
+	}
+	
+	//m_mediaCaptureMgr->StartPreviewAsync();
+
+	create_task(m_mediaCaptureMgr->ClearEffectsAsync(Windows::Media::Capture::MediaStreamType::VideoPreview)).then([this](task<void> cleanTask)
+	{
+		//cleanTask.get();
+
+	}).then([this](){
+
+						PropertySet^ props = ref new PropertySet();
+						props->Insert(L"{698649BE-8EAE-4551-A4CB-3EC98FBD3D86}", mode);
+						create_task(m_mediaCaptureMgr->AddEffectAsync(Windows::Media::Capture::MediaStreamType::VideoPreview, "OcvTransform.OcvImageManipulations", props)).then([this](task<void> effectTask)
+						{
+			
+							try
+							{
+								//effectTask.get();
+														
+
+							}
+							catch (Exception ^e)
+							{
+								ShowStatusMessage(e->Message);
+
+							}
+						});
+	});
+
+
+	mode++;
+	if (mode == 6)
+		mode = 0;
+}
+
+
+void ComicLife2::MainPage::tmpFct(){
+
+	auto mediaCapture = m_mediaCaptureMgr.Get();
+	preview1->Source = mediaCapture;
+
+	create_task(mediaCapture->StartPreviewAsync()).then([this](task<void> previewTask)
+	{
+		try
+		{
+			previewTask.get();
+
+			ShowStatusMessage("Start preview successful");
+		}
+		catch (Exception ^e)
+		{
+			ShowStatusMessage(e->Message);
+		}
+	});
+
+
+}
+
+
+void ComicLife2::MainPage::tmpFct2(){
+	auto mediaCapture = m_mediaCaptureMgr.Get();
+
+	//Windows::Media::Capture::VideoDeviceCharacteristic charecteristic = mediaCapture->MediaCaptureSettings->VideoDeviceCharacteristic;
+
+	create_task(m_mediaCaptureMgr->ClearEffectsAsync(Windows::Media::Capture::MediaStreamType::VideoPreview)).then([this](task<void> cleanTask)
+	{
+		preview1->Source = m_mediaCaptureMgr.Get();
+
+
+
+		PropertySet^ props = ref new PropertySet();
+		props->Insert(L"{698649BE-8EAE-4551-A4CB-3EC98FBD3D86}", 2);
+		create_task(m_mediaCaptureMgr->AddEffectAsync(Windows::Media::Capture::MediaStreamType::VideoPreview, "OcvTransform.OcvImageManipulations", props)).then([this](task<void> effectTask)
+		{
+
+			try
+			{
+				effectTask.get();
+				auto mediaCapture = m_mediaCaptureMgr.Get();
+				m_mediaCaptureMgr->StartPreviewAsync();
+				/*task<void>(mediaCapture->AddEffectAsync(Windows::Media::Capture::MediaStreamType::VideoRecord, "OcvTransform.OcvImageManipulations", nullptr)).then([this](task<void> effectTask2)
+				{
+				try
+				{
+				effectTask2.get();
+				ShowStatusMessage("Add effect successful to record stream successful");
+
+				AddEffectToImageStream();
+
+				}
+				catch (Exception ^e)
+				{
+				ShowStatusMessage(e->Message);
+
+				}
+				});
+				*/
+
+			}
+			catch (Exception ^e)
+			{
+				ShowStatusMessage(e->Message);
+
+			}
+		});
+	});
+
+}
+
+
---
+++ ComicLife2/ComicLife2/ComicLife2.Windows/MainPage.xaml.h
@@ -0,0 +1,50 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+#include <opencv2\imgproc\types_c.h>
+
+using namespace concurrency;
+using namespace Windows::Devices::Enumeration;
+
+namespace ComicLife2
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+		void LoadImage();
+		
+	private:
+		void UpdateImage(cv::Mat image);
+		int frameWidth;
+		int frameHeight;
+		cv::Mat Lena;
+		Platform::Agile<Windows::Media::Capture::MediaCapture> m_mediaCaptureMgr;
+		DeviceInformationCollection^ m_devInfoCollection;
+		void EnumerateWebcamsAndInitializeFirstOneAsync();
+		
+		void ShowStatusMessage(Platform::String^ text);
+		cv::String ComicLife2::MainPage::ConvertPath(Platform::String^ path);
+		void tmpFct();
+		void tmpFct2();
+
+		bool m_bPreviewing;
+		int mode;
+
+	private:
+		void btn1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void btn2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void btn3_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void btn4_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
